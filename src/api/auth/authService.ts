6660af1c48ebf1292b8ddcc832d160abb557c24e
lforjan <lucija.forjan@cinnamon.agency>
2022-05-19 14:07:27 +0200

    add hashPassword method

--- src/api/auth/authService.ts
+++ src/api/auth/authService.ts
@@ -1,8 +1,17 @@
 import { IAuthService } from "./interface";
+const bcrypt = require('bcrypt');
 
 export class AuthService implements IAuthService {
     hashPassword(password: string): Promise<string> {
-        throw new Error("Method not implemented.");
+        return new Promise((resolve, reject) => {
+            bcrypt.genSalt(process.env.SALT_ROUNDS), (err: Object, salt: string) => {
+                if (err) reject(err);
+                bcrypt.hash(password, salt, (err: Object, hash: string) => {
+                  if (err) reject(err);
+                  else resolve(hash);
+                });
+              };
+        })
     }
     generateJwtToken(email: string, password: string): string {
         throw new Error("Method not implemented.");
