cdc5d1f26eca985170f98a7ae7321cea0137d74c
ime prezime <keva@imes-MacBook-Pro.local>
2022-05-19 15:59:51 +0200

    add createUser method

---
+++ src/api/auth/authRouter.ts
@@ -0,0 +1,7 @@
+import express from 'express';
+import { AuthController } from './authController';
+
+const authController = new AuthController()
+export const authRouter = express.Router();
+
+authRouter.get('/signup', authController.signUp)
