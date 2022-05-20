22632b33b9da4e415da3687425c6a9eb387984a7
andrija <andrija.kevic@cinnamon.agency>
2022-05-20 14:51:43 +0200

    add auth route

--- src/api/auth/authController.ts
+++ src/api/auth/authController.ts
@@ -1,4 +1,4 @@
-import e, { Request, Response, NextFunction } from 'express';
+import { Request, Response, NextFunction } from 'express';
 import { AuthService } from "./authService";
 
 export class AuthController {
@@ -9,9 +9,38 @@ export class AuthController {
     }
 
     signUp = async (req: Request, res: Response, next: NextFunction) => {
+        const email = req.query.email
+        const pass = req.query.password
         try {
-            const user = await this.authService.createUser('email', 'password')
+            const user = await this.authService.createUser(email + '', pass + '')
             res.send(user)
+        } catch (err) {
+            throw err
+        }
+    }
+
+    signIn = async (req: Request, res: Response, next: NextFunction) => {
+        const email = req.query.email
+        const password = req.query.password
+        try {
+            const user = await this.authService.checkCreds(email + '', password + '')
+
+            if (!user) {
+                res.status(404).send('User not found')
+            }
+
+            const token = this.authService.generateJwtToken(user![0].email, user![0].password)
+
+            res.set('auth-token', token)
+
+            return res.status(200).send({
+                data: {
+                    user,
+                    token,
+                },
+                code: 200,
+                message: 'OK'
+            })
         } catch (err) {
             throw err
         }
