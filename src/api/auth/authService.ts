53914587f8290afb739b34a40b41def2db4cf31f
Lucijan Pavic <lucijan.pavic@cinnamon.agency>
2022-06-09 15:34:03 +0200

    fix Error: User is already exist

--- src/api/auth/authService.ts
+++ src/api/auth/authService.ts
@@ -1,71 +1,75 @@
-import { IAuthService } from "./interface";
-import bcrypt from 'bcrypt';
-import jwt from 'jsonwebtoken';
-import { User } from "../user/userModel";
-import { UserType } from "../user/interface";
+import { IAuthService } from './interface'
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+import { User } from '../user/userModel'
+import { UserType } from '../user/interface'
 
 export class AuthService implements IAuthService {
-    async hashPassword(password: string): Promise<string> {
-        const salt = await bcrypt.genSalt(10)
+	async hashPassword(password: string): Promise<string> {
+		const salt = await bcrypt.genSalt(10)
 
-        const hash = await bcrypt.hash(password, salt)
+		const hash = await bcrypt.hash(password, salt)
 
-        return hash
-    }
+		return hash
+	}
 
-    generateJwtToken(email: string, password: string): string {
-            const timestamp = Date.now() / 1000;
-            const token = jwt.sign(
-            {
-                exp: Math.floor(timestamp + 60 * 120),
-                iat: timestamp,
-                email,
-                password
-            },
-            process.env.JWT_SECRET!
-        )
-        return token;
-    }
+	generateJwtToken(email: string, password: string): string {
+		const timestamp = Date.now() / 1000
+		const token = jwt.sign(
+			{
+				exp: Math.floor(timestamp + 60 * 120),
+				iat: timestamp,
+				email,
+				password
+			},
+			process.env.JWT_SECRET!
+		)
+		return token
+	}
 
-    public static verifyJwtToken(token: string): any {
-        return jwt.verify(token, process.env.JWT_SECRET!)
-    }
+	public static verifyJwtToken(token: string): any {
+		return jwt.verify(token, process.env.JWT_SECRET!)
+	}
 
-    async createUser (email: string, fullName: string, password: string): Promise<User> {
-        const passwordHash = await this.hashPassword(password)
+	async createUser(email: string, fullName: string, password: string): Promise<User> {
+		const passwordHash = await this.hashPassword(password)
+		const userEmail = await this.checkEmail(email)
 
-        const user = await User.create({       
-            role_id: '1',
-            full_name: fullName,
-            email: email, 
-            password: passwordHash, 
-            default_currency_id: '1'
-        })
+		if (userEmail) {
+			throw new Error('User already exist')
+		}
+		const user = await User.create({
+			role_id: '1',
+			full_name: fullName,
+			email: email,
+			password: passwordHash,
+			default_currency_id: '1'
+		})
 
-        return user as User
-    }
+		return user as User
+	}
 
-    async checkCreds (email: string, password: string): Promise<UserType | undefined> {
-        const user = await this.checkEmail(email)
-        const isValid = await this.checkPass(password, user.password)
+	async checkCreds(email: string, password: string): Promise<UserType | undefined> {
+		const user = await this.checkEmail(email)
+		const isValid = await this.checkPass(password, user.password)
 
-        if (!isValid) {
-            return
-        }
+		if (!isValid) {
+			return
+		}
 
-        return user
-    }
+		return user
+	}
 
-    private async checkEmail (email: string): Promise<UserType> {
-        const user = await User.findOne({
-            where: {
-                email: email
-            }
-        })
-        return user as any
-    }
+	private async checkEmail(email: string): Promise<UserType> {
+		const user = await User.findOne({
+			where: {
+				email: email
+			}
+		})
+		return user as any
+	}
 
-    private async checkPass (pass: string, hashedPass: string): Promise<boolean> {
-        return bcrypt.compare(pass, hashedPass)
-    }
+	private async checkPass(pass: string, hashedPass: string): Promise<boolean> {
+		return bcrypt.compare(pass, hashedPass)
+	}
 }
