2f7f4cccc10fe135eb31054ffc98bfb80e2175b1
andrija <andrija.kevic@cinnamon.agency>
2022-06-13 12:45:54 +0200

    update error handling

--- src/api/helper/errorHandler.ts
+++ src/api/helper/errorHandler.ts
@@ -4,11 +4,32 @@ export enum HttpStatusCodes {
 	INTERNAL_SERVER_ERROR = 500
 }
 
+export interface ErrorPayload {
+	errorCode?: string;
+	httpStatusCode?: number;
+	finalClosure?: () => Promise<void>;
+}
+
+export class GffError extends Error {
+	finalClosure?: () => Promise<void>;
+	httpStatusCode?: number;
+	errorCode?: string;
+
+	constructor(message: string, errorPayload?: ErrorPayload) {
+		super(message);
+		this.finalClosure = errorPayload?.finalClosure;
+		this.httpStatusCode = errorPayload?.httpStatusCode;
+		this.errorCode = errorPayload?.errorCode;
+		this.name = this.constructor.name;
+	}
+}
+
+
 export const jsonErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
 	if (typeof err.code === 'string' && (err.code.startsWith('42') || err.code.startsWith('22'))) {
 		err.message = 'Internal server error: Database error during request'
 	}
-	const { errorCode, httpStatusCode } = err
+	const { errorCode, httpStatusCode } = err as GffError
 
 	res.statusCode = httpStatusCode ?? HttpStatusCodes.INTERNAL_SERVER_ERROR
 	return res.json({
