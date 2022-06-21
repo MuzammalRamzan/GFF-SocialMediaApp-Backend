286c404442106032ee311d738ac9f3659f7f0793
Kaushal Koladiya <kaushalkoladiya123@gmail.com>
2022-06-21 18:29:05 +0530

    Chat rooms and messages

---
+++ src/api/chat/room/interface.ts
@@ -0,0 +1,13 @@
+import { Room } from "./room.model";
+
+export type RoomParams = {
+  name: string;
+  user_ids?: string[];
+}
+
+export interface IRoomService {
+  getRoom(id: string): Promise<Room>;
+  getRooms(): Promise<Room[]>;
+  createRoom(params: RoomParams): Promise<Room>;
+  joinRoom(room_id: number, user_id: number): Promise<Room>;
+}
