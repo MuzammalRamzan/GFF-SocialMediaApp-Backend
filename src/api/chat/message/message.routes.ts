4c04a00eaad67776dab4925a2ff2990f7238ea9c
Kaushal Koladiya <kaushalkoladiya123@gmail.com>
2022-07-05 21:35:55 +0530

    feat/ implemented  long-polling

--- src/api/chat/message/message.routes.ts
+++ src/api/chat/message/message.routes.ts
@@ -1,9 +1,10 @@
-import express, { Application } from 'express';
-import { authMiddleware } from '../../helper/authMiddleware';
-import { MessageController } from './message.controller';
+import express, { Application } from 'express'
+import { authMiddleware } from '../../helper/authMiddleware'
+import { MessageController } from './message.controller'
 
 const controller = new MessageController()
-export const messageRoute = express.Router();
+export const messageRoute = express.Router()
 
-messageRoute.post('/send/room/:id', authMiddleware, controller.sendMessage as Application);
-messageRoute.get('/room/:id', authMiddleware, controller.getMessages as Application);
+messageRoute.post('/send/room/:id', authMiddleware, controller.sendMessage as Application)
+messageRoute.get('/room/:id', authMiddleware, controller.getMessages as Application)
+messageRoute.get('/room/subscribe/:id', authMiddleware, controller.subscribeToRoom as Application)
--- src/api/chat/message/message.service.ts
+++ src/api/chat/message/message.service.ts
@@ -1,42 +1,80 @@
-import { Op } from "sequelize";
-import { UserInformation } from "../../user-information/userInformationModel";
-import { User } from "../../user/userModel";
-import { Message } from "./message.model";
+import { Request, Response } from 'express'
+import { Op } from 'sequelize'
+import { UserInformation } from '../../user-information/userInformationModel'
+import { User } from '../../user/userModel'
+import { Message } from './message.model'
+
+type SubscribersType = {
+	[room_id: string]: { [user_id: string]: Response }
+}
 
 export class MessageService {
-  public async sendMessage(body: string, user_id: number, room_id: number): Promise<Message> {
-    return await Message.create({
-      body,
-      user_id,
-      room_id
-    });
-  }
+	private subscribers: SubscribersType = Object.create(null)
 
-  public async getMessages(room_id: number, from: string): Promise<Message[]> {
-    const from_date = new Date(from);
+	public async sendMessage(body: string, user_id: number, room_id: number): Promise<Message> {
+		const message = await Message.create({
+			body,
+			user_id,
+			room_id
+		})
 
-    return await Message.findAll({
-      where: {
-        room_id,
-        created_at: {
-          [Op.gte]: from_date
-        }
-      },
-      order: [["created_at", "ASC"]],
-      include: [
-        {
-          model: User,
-          as: "user",
-          attributes: ["id", "full_name"],
-          include: [
-            {
-              model: UserInformation,
-              as: "user_information",
-              attributes: ["profile_url"]
-            }
-          ]
-        }
-      ]
-    });
-  }
+		this.publishMessage(body, user_id, room_id)
+
+		return message
+	}
+
+	public async getMessages(room_id: number, from: string): Promise<Message[]> {
+		const from_date = new Date(from)
+
+		return await Message.findAll({
+			where: {
+				room_id,
+				created_at: {
+					[Op.gte]: from_date
+				}
+			},
+			order: [['created_at', 'ASC']],
+			include: [
+				{
+					model: User,
+					as: 'user',
+					attributes: ['id', 'full_name'],
+					include: [
+						{
+							model: UserInformation,
+							as: 'user_information',
+							attributes: ['profile_url']
+						}
+					]
+				}
+			]
+		})
+	}
+
+	public async subscribeToRoom(
+		req: Request,
+		res: Response,
+		params: { user_id: number; room_id: number }
+	): Promise<void> {
+		const { user_id, room_id } = params
+
+		res.setHeader('Content-Type', 'application/json;charset=utf-8')
+		res.setHeader('Cache-Control', 'no-cache, must-revalidate')
+
+		this.subscribers[room_id][user_id] = res
+
+		req.on('close', () => {
+			delete this.subscribers[room_id][user_id]
+		})
+	}
+
+	public async publishMessage(body: string, user_id: number, room_id: number): Promise<void> {
+		let roomParticipants = Object.keys(this.subscribers[room_id]).filter(userId => +userId !== user_id)
+
+		roomParticipants.map(participantId => {
+			this.subscribers[room_id][participantId].end(JSON.stringify({ user_id, message: body }))
+		})
+
+		this.subscribers[room_id] = Object.create(null)
+	}
 }
