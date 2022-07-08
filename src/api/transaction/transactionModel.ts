1f39a8dc2ac60cf1c9a0a749c8067ff33602c6f5
andrija <andrija.kevic@cinnamon.agency>
2022-07-08 12:41:08 +0200

    add missing fields to transaction

--- src/api/transaction/transactionModel.ts
+++ src/api/transaction/transactionModel.ts
@@ -22,6 +22,10 @@ Transaction.init(
 			type: DataTypes.INTEGER,
 			allowNull: false
 		},
+		ammount: {
+			type: DataTypes.INTEGER,
+			allowNull: false
+		},
 		category_id: {
 			type: DataTypes.INTEGER,
 			allowNull: false
