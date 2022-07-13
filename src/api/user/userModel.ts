3a164e9e69b315c8533105c7aa07e4c20929a92a
Kaushal Koladiya <kaushalkoladiya123@gmail.com>
2022-07-13 19:10:23 +0530

    feat/ added get all warriors api

--- src/api/user/userModel.ts
+++ src/api/user/userModel.ts
@@ -1,74 +1,77 @@
-import { DataTypes, Model } from 'sequelize';
-import { sequelize } from '../../database';
-import { WarriorInformation } from '../warrior-information/warriorInformationModel';
-import { MentorInformation } from '../mentor-information/mentorInformationModel';
-import { UserInformation } from '../user-information/userInformationModel';
+import { DataTypes, Model } from 'sequelize'
+import { sequelize } from '../../database'
+import { WarriorInformation } from '../warrior-information/warriorInformationModel'
+import { MentorInformation } from '../mentor-information/mentorInformationModel'
+import { UserInformation } from '../user-information/userInformationModel'
 
 export interface IUser {
-    id: number;
-    email: string;
-    firstname: string;
-    lastname: string;
-    phone_number: string;
-    default_currency_id: number;
-    role_id: number;
-    password: string;
+	id: number
+	email: string
+	firstname: string
+	lastname: string
+	phone_number: string
+	default_currency_id: number
+	role_id: number
+	password: string
 }
 
-export class User extends Model { }
-User.init({
-    id: {
-        type: DataTypes.INTEGER,
-        autoIncrement: true,
-        primaryKey: true,
-        allowNull: false,
-    },
-    role_id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-    },
-    full_name: {
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
-    email: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true
-    },
-    password: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-    default_currency_id: {
-        type: DataTypes.INTEGER,
-        defaultValue: 1
-    },
-    user_feature_id: {
-        type: DataTypes.INTEGER,
-        allowNull: true,
-        defaultValue: null
-    }
-},
-    {
-        sequelize,
-        tableName: 'user',
-        timestamps: true
-    })
+export class User extends Model {}
+User.init(
+	{
+		id: {
+			type: DataTypes.INTEGER,
+			autoIncrement: true,
+			primaryKey: true,
+			allowNull: false
+		},
+		role_id: {
+			type: DataTypes.INTEGER,
+			allowNull: false
+		},
+		full_name: {
+			type: DataTypes.STRING,
+			allowNull: false
+		},
+		email: {
+			type: DataTypes.STRING,
+			allowNull: false,
+			unique: true
+		},
+		password: {
+			type: DataTypes.STRING,
+			allowNull: false
+		},
+		default_currency_id: {
+			type: DataTypes.INTEGER,
+			defaultValue: 1
+		},
+		user_feature_id: {
+			type: DataTypes.INTEGER,
+			allowNull: true,
+			defaultValue: null
+		}
+	},
+	{
+		sequelize,
+		tableName: 'user',
+		timestamps: true
+	}
+)
 
 User.hasOne(WarriorInformation, {
-    foreignKey: 'user_id',
-    as: 'warrior_information'
-});
+	foreignKey: 'user_id',
+	as: 'warrior_information'
+})
+WarriorInformation.belongsTo(User, { foreignKey: 'user_id', as: 'user' })
 
 User.hasOne(MentorInformation, {
-    foreignKey: 'user_id',
-    as: 'mentor_information'
+	foreignKey: 'user_id',
+	as: 'mentor_information'
 })
 
 User.hasOne(UserInformation, {
-    foreignKey: 'user_id',
-    as: 'user_information'
+	foreignKey: 'user_id',
+	as: 'user_information'
 })
 
-User.sync();
+User.sync()
--- src/api/warrior-information/warriorInformationService.ts
+++ src/api/warrior-information/warriorInformationService.ts
@@ -1,110 +1,120 @@
-import { GffError } from "../helper/errorHandler";
-import { UserInformation } from "../user-information/userInformationModel";
-import { UserRoleService } from "../user-role/userRoleService";
-import { User } from "../user/userModel";
-import { WellnessWarriorService } from "../wellness-warrior/wellnessWarriorService";
-import { IWarriorInformationService, IWarriorUser, WarriorInformationParams } from "./interface";
-import { WarriorInformation } from "./warriorInformationModel";
+import { GffError } from '../helper/errorHandler'
+import { UserInformation } from '../user-information/userInformationModel'
+import { UserRoleService } from '../user-role/userRoleService'
+import { User } from '../user/userModel'
+import { WellnessWarriorService } from '../wellness-warrior/wellnessWarriorService'
+import { IWarriorInformationService, IWarriorUser, WarriorInformationParams } from './interface'
+import { WarriorInformation } from './warriorInformationModel'
 
 export class WarriorInformationService implements IWarriorInformationService {
-  constructor() { }
+	constructor() {}
 
-  private createOrUpdate = async (params: WarriorInformationParams): Promise<WarriorInformation> => {
-    const warriorInformation = await WarriorInformation.findOne({
-      where: {
-        user_id: params.user_id
-      }
-    });
+	private createOrUpdate = async (params: WarriorInformationParams): Promise<WarriorInformation> => {
+		const warriorInformation = await WarriorInformation.findOne({
+			where: {
+				user_id: params.user_id
+			}
+		})
 
-    const payload = {
-      specialty: params.specialty.join(','),
-      certification: params.certification.join(','),
-      therapy_type: params.therapy_type.join(','),
-      price_range: params.price_range.join(','),
-    }
+		const payload = {
+			specialty: params.specialty.join(','),
+			certification: params.certification.join(','),
+			therapy_type: params.therapy_type.join(','),
+			price_range: params.price_range.join(',')
+		}
 
-    if (warriorInformation) {
-      return await warriorInformation.update(payload);
-    } else {
-      return await WarriorInformation.create(payload);
-    }
-  }
+		if (warriorInformation) {
+			return await warriorInformation.update(payload)
+		} else {
+			return await WarriorInformation.create(payload)
+		}
+	}
 
-  getById = async (user_id: number): Promise<IWarriorUser> => {
-    const record = await User.findOne({
-      where: {
-        id: user_id
-      },
-      attributes: ['id', 'full_name'],
-      include: [
-        {
-          model: WarriorInformation,
-          as: "warrior_information",
-          attributes: ["specialty", "certification", "therapy_type", "price_range"]
-        },
-        {
-          model: UserInformation,
-          as: "user_information",
-          attributes: ["profile_url", "bio", "date_of_birth", "gender", "job_role", "education"]
-        }
-      ]
-    });
+	getById = async (user_id: number): Promise<IWarriorUser> => {
+		const record = await User.findOne({
+			where: {
+				id: user_id
+			},
+			attributes: ['id', 'full_name'],
+			include: [
+				{
+					model: WarriorInformation,
+					as: 'warrior_information',
+					attributes: ['specialty', 'certification', 'therapy_type', 'price_range']
+				},
+				{
+					model: UserInformation,
+					as: 'user_information',
+					attributes: ['profile_url', 'bio', 'date_of_birth', 'gender', 'job_role', 'education']
+				}
+			]
+		})
 
-    if (!record) {
-      const error = new GffError('Wellness Warrior information not found!')
-      error.errorCode = '404'
-      throw error
-    }
+		if (!record) {
+			const error = new GffError('Wellness Warrior information not found!')
+			error.errorCode = '404'
+			throw error
+		}
 
-    const wellness_warrior_request = await WellnessWarriorService.getWarriorRequestByUserId(user_id);
+		const wellness_warrior_request = await WellnessWarriorService.getWarriorRequestByUserId(user_id)
 
-    const user = record?.get();
+		const user = record?.get()
 
-    return {
-      id: user.id,
-      full_name: user.full_name,
-      user_information: user.user_information,
-      warrior_information: {
-        specialty: user.warrior_information?.specialty.split(','),
-        certification: user.warrior_information?.certification.split(','),
-        therapy_type: user.warrior_information?.therapy_type.split(','),
-        price_range: user.warrior_information?.price_range.split(','),
-      },
-      wellness_warrior_request: wellness_warrior_request
-    }
-  }
+		return {
+			id: user.id,
+			full_name: user.full_name,
+			user_information: user.user_information,
+			warrior_information: {
+				specialty: user.warrior_information?.specialty.split(','),
+				certification: user.warrior_information?.certification.split(','),
+				therapy_type: user.warrior_information?.therapy_type.split(','),
+				price_range: user.warrior_information?.price_range.split(',')
+			},
+			wellness_warrior_request: wellness_warrior_request
+		}
+	}
 
-  create = async (params: WarriorInformationParams): Promise<WarriorInformation> => {
-    const warriorInformation = await this.createOrUpdate(params);
+	create = async (params: WarriorInformationParams): Promise<WarriorInformation> => {
+		const warriorInformation = await this.createOrUpdate(params)
 
-    const warriorRole = await UserRoleService.fetchWellnessWarriorRole();
+		const warriorRole = await UserRoleService.fetchWellnessWarriorRole()
 
-    await User.update({
-      role_id: warriorRole?.get('id'),
-    }, {
-      where: {
-        id: params.user_id
-      }
-    })
+		await User.update(
+			{
+				role_id: warriorRole?.get('id')
+			},
+			{
+				where: {
+					id: params.user_id
+				}
+			}
+		)
 
-    return warriorInformation.get();
-  }
+		return warriorInformation.get()
+	}
 
-  update = async (params: WarriorInformationParams): Promise<WarriorInformation | null> => {
-    const record = await this.createOrUpdate(params);
+	update = async (params: WarriorInformationParams): Promise<WarriorInformation | null> => {
+		const record = await this.createOrUpdate(params)
 
-    return record ? await WarriorInformation.findOne({ where: { user_id: params.user_id } }) : null;
-  }
+		return record ? await WarriorInformation.findOne({ where: { user_id: params.user_id } }) : null
+	}
 
-  static isUserWarrior = async (user_id: number): Promise<boolean> => {
-    const warriorRole = await UserRoleService.fetchWellnessWarriorRole();
-    const record = await User.findOne({
-      where: {
-        id: user_id,
-        role_id: warriorRole?.get('id')
-      }
-    });
+	async getAll(): Promise<WarriorInformation[]> {
+		const warriors = await WarriorInformation.findAll({
+			include: [{ model: User, as: 'user', attributes: { exclude: ['password'] } }]
+		})
+		return warriors
+	}
 
-    return !!record?.get();
-  }
+	static isUserWarrior = async (user_id: number): Promise<boolean> => {
+		const warriorRole = await UserRoleService.fetchWellnessWarriorRole()
+		const record = await User.findOne({
+			where: {
+				id: user_id,
+				role_id: warriorRole?.get('id')
+			}
+		})
+
+		return !!record?.get()
+	}
 }
