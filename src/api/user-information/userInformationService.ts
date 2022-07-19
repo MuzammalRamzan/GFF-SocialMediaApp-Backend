3b884e53aa5aede7ff11feff75e1eb1a7f51342b
Kaushal Koladiya <kaushalkoladiya123@gmail.com>
2022-07-19 09:54:13 +0530

    Change update user info flow

--- src/api/user-information/userInformationService.ts
+++ src/api/user-information/userInformationService.ts
@@ -36,41 +36,18 @@ export class UserInformationService implements IUserInformationService {
 	}
 
 	async update(userId: number, params: UserInformationType): Promise<UserInformation> {
-		await UserInformation.update(
-			{
-				bio: params.bio,
-				date_of_birth: params.date_of_birth,
-				gender: params.gender,
-				email: params.email,
-				phone_number: params.phone_number,
-				country: params.country,
-				city: params.city,
-				state: params.state,
-				zip_code: params.zip_code,
-				address: params.address,
-				twitter: params.twitter,
-				facebook: params.facebook,
-				linkedin: params.linkedin,
-				instagram: params.instagram,
-				tiktok: params.tiktok,
-				employer_name: params.employer_name,
-				job_role: params.job_role,
-				education: params.education,
-				other_education: params.other_education
-			},
-			{
-				where: {
-					user_id: userId
-				}
-			}
-		)
-
-		const updatedRow = await UserInformation.findOne({
+		const userInformation = await UserInformation.findOne({
 			where: {
-				user_id: params.user_id
+				user_id: userId
 			}
-		})
-		return updatedRow as UserInformation
+		});
+
+		if (userInformation) {
+			userInformation.set(params);
+			return await userInformation.save();
+		} else {
+			return await UserInformation.create(params);
+		}
 	}
 
 	async delete(params_user_id: number, userId: number): Promise<number> {
