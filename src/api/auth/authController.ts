d26e3afd27c39cd917461e9d27c1200eb7eb24e1
Kaushal Koladiya <kaushalkoladiya123@gmail.com>
2022-07-19 21:21:59 +0530

    Added admin login route

--- src/api/auth/authController.ts
+++ src/api/auth/authController.ts
@@ -1,6 +1,7 @@
 import { Request, Response, NextFunction } from 'express'
 import { Messages } from '../../constants/messages'
 import { GffError, jsonErrorHandler } from '../helper/errorHandler'
+import { UserRoleService } from '../user-role/userRoleService'
 import { AuthService } from './authService'
 
 export class AuthController {
@@ -27,7 +28,7 @@ export class AuthController {
 			const error = err as GffError
 			error.errorCode = '400'
 			error.httpStatusCode = 400
-			return jsonErrorHandler(err, req, res, () => {})
+			return jsonErrorHandler(err, req, res, () => { })
 		}
 	}
 
@@ -64,7 +65,46 @@ export class AuthController {
 			if (!error?.httpStatusCode) {
 				error.httpStatusCode = 404
 			}
-			return jsonErrorHandler(err, req, res, () => {})
+			return jsonErrorHandler(err, req, res, () => { })
+		}
+	}
+
+	adminLogIn = async (req: Request, res: Response, next: NextFunction) => {
+		const email = req.body.email
+		const password = req.body.password
+		try {
+			const user = await this.authService.checkCreds(email, password)
+
+			const adminRole = await UserRoleService.fetchAdminRole();
+
+			if (user?.role_id !== adminRole?.get('id')) {
+				const err = new GffError("User not found!")
+				err.errorCode = '404'
+				err.httpStatusCode = 404
+				throw err
+			}
+
+			const token = this.authService.generateJwtToken(user!.email, user!.password)
+
+			res.set('auth-token', token)
+
+			return res.status(200).send({
+				data: {
+					user,
+					token
+				},
+				code: 200,
+				message: 'OK'
+			})
+		} catch (err) {
+			const error = err as GffError
+			if (!error?.errorCode) {
+				error.errorCode = '404'
+			}
+			if (!error?.httpStatusCode) {
+				error.httpStatusCode = 404
+			}
+			return jsonErrorHandler(err, req, res, () => { })
 		}
 	}
 }
